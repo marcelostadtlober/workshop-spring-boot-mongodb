056f05b917478eea7984df46a5d95c65e7118655
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2018-06-26 14:43:16 -0300

    Obtendo um post por id

---
+++ src/main/java/com/mstadtlober/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.mstadtlober.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mstadtlober.workshopmongo.domain.Post;
+import com.mstadtlober.workshopmongo.repository.PostRepository;
+import com.mstadtlober.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
